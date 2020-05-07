6f2383cfe19c8bde9977fdcf6fdc77eee5cc66f5
reinvald <reinvald@seas.upenn.edu>
2020-05-07 14:38:20 -0400

    setup Selenium ChromeDriver to fetch tickers of the five biggest gainers of the day

---
+++ main.py
@@ -0,0 +1,44 @@
+import time
+from selenium import webdriver
+
+
+# navigate to Yahoo Finance using Selenium ChromeDriver to get top five gainers
+def get_stocks():
+
+    print('fetching top five stocks for today...')
+    driver = webdriver.Chrome()
+    driver.get("https://finance.yahoo.com/gainers")
+
+    # XPath for "Change" filter
+    changes = driver.find_element_by_xpath(
+        '//*[@id="scr-res-table"]/div[1]/table/thead/tr/th[4]')
+
+    # filter by decreasing Change
+    changes.click()
+    time.sleep(2)
+    changes.click()
+    time.sleep(2)
+
+    # print(str(driver.find_element_by_xpath(
+    #   '//*[@id="scr-res-table"]/div[1]/table/tbody/tr[1]/td[1]/a')))
+
+    # get tickers for top five stocks
+    stocks = []
+    stocks.append(driver.find_element_by_xpath(
+        '//*[@id="scr-res-table"]/div[1]/table/tbody/tr[1]/td[1]/a').text)
+    stocks.append(driver.find_element_by_xpath(
+        '//*[@id="scr-res-table"]/div[1]/table/tbody/tr[2]/td[1]/a').text)
+    stocks.append(driver.find_element_by_xpath(
+        '//*[@id="scr-res-table"]/div[1]/table/tbody/tr[3]/td[1]/a').text)
+    stocks.append(driver.find_element_by_xpath(
+        '//*[@id="scr-res-table"]/div[1]/table/tbody/tr[4]/td[1]/a').text)
+    stocks.append(driver.find_element_by_xpath(
+        '//*[@id="scr-res-table"]/div[1]/table/tbody/tr[5]/td[1]/a').text)
+
+    print('today\'s stocks: ' + stocks[1] + ', ' + stocks[1] +
+          ', ' + stocks[2] + ', ' + stocks[3] + ', ' + stocks[4])
+    driver.quit()
+
+
+if __name__ == "__main__":
+    get_stocks()

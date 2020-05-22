6c7d5e2e3a33905ae933cdb1cf504b3f7d95eaaf
reinvald <reinvald@seas.upenn.edu>
2020-05-22 13:21:07 -0400

    updated webscraper

--- webscraper.py
+++ webscraper.py
@@ -7,10 +7,69 @@ import time
 class WebScraper:
 
     def __init__(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.stocks = self.get_stocks()
 
     # method for fetching stocks
     def get_stocks(self):
+        print('fetching top five stocks for today...')
+        self.driver.get("https://finance.yahoo.com/gainers")
+        time.sleep(3)
+
+        # XPath for "Change" filter
+        changes = self.driver.find_element_by_xpath(
+            '//*[@id="scr-res-table"]/div[1]/table/thead/tr/th[4]')
+
+        # filter by decreasing Change
+        changes.click()
+        time.sleep(3)
+        changes.click()
+        time.sleep(3)
+
+        # get tickers for top five NYSE stocks
+        stocks = []
+        curr_stock_ctr = 1
+
+        while len(stocks) != 5:
+
+            # click stock
+            curr_stock = self.driver.find_element_by_xpath(
+                '//*[@id="scr-res-table"]/div[1]/table/tbody/tr[' + str(curr_stock_ctr) + ']/td[1]/a')
+
+            curr_stock_ticker = curr_stock.text
+
+            curr_stock.click()
+
+            time.sleep(5)
+            while not self.page_has_loaded():
+                time.sleep(5)
+
+            # market information
+            market_info = self.driver.find_element_by_xpath(
+                '//*[@id="quote-header-info"]/div[2]/div[1]/div[2]/span').text
+
+            if (market_info[0:4] == 'NYSE'):
+                stocks.append(curr_stock_ticker)
+
+            curr_stock_ctr += 1
+
+            self.driver.execute_script("window.history.go(-1)")
+
+            time.sleep(5)
+            while not self.page_has_loaded():
+                time.sleep(5)
+
+        print('today\'s top five gainers: ' + stocks[0] + ', ' + stocks[1] +
+              ', ' + stocks[2] + ', ' + stocks[3] + ', ' + stocks[4])
+        self.driver.quit()
+
+        return stocks
+
+    def page_has_loaded(self):
+        page_state = self.driver.execute_script('return document.readyState;')
+        return page_state == 'complete'
+
+    def old_get_stocks(self):
         print('fetching top five stocks for today...')
         driver = webdriver.Chrome(ChromeDriverManager().install())
         driver.get("https://finance.yahoo.com/gainers")

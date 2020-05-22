73d406413a115fd2a429808b54e38b760b201a42
reinvald <reinvald@seas.upenn.edu>
2020-05-22 16:12:33 -0400

    added caffeine to prevent connection loss during trading

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from tradeengine import TradeEngine
+import caffeine
 
 # main script
 if __name__ == "__main__":
 
+    # enable caffeine to prevent connection loss during trading
+    caffeine.on(display=False)
+
     # start trade engine
     te = TradeEngine()
+
+    # turn off caffeine so that system can sleep after script finishes
+    caffeine.off()
